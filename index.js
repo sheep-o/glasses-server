11e3fd46bddaa567b3ee6cc0f052d96d5919a28e
sheepo <47284480+sheep-o@users.noreply.github.com>
2022-10-19 08:48:43 -0400

    HELLO

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const app = express()
 const server = require('http').Server(app)
 const { Server } = require("socket.io");
 const fs = require('fs')
-
-const tesseract = require("node-tesseract-ocr")
+const jimp = require('jimp')
+//const tesseract = require("node-tesseract-ocr")
+const NodeWebcam = require('node-webcam')
+const opts = {
+  width: 1280,
+  height: 720,
+  quality: 10,
+  frames: 1,
+  delay: 0,
+  saveShots: false,
+  callbackReturn: 'buffer',
+  device: false
+}
+const Webcam = NodeWebcam.create( opts )
 
 const io = new Server(server);
 
@@ -14,21 +26,29 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   console.log('user connected')
+  
+  // const config = {
+  //   lang: "eng"
+  // }
 
-  const config = {
-    lang: "eng"
-  }
-
-  tesseract
-    .recognize('./eng_bw.png', config)
-    .then((text) => {
-      console.log("Result:", text)
-    })
-    .catch((error) => {
-      console.log(error.message)
-    })
+  // tesseract
+  //   .recognize('./eng_bw.png', config)
+  //   .then((text) => {
+  //     console.log("Result:", text)
+  //   })
+  //   .catch((error) => {
+  //     console.log(error.message)
+  //   })
 
 })
+
+function detectMimeType(b64) {
+  for (var s in signatures) {
+    if (b64.indexOf(s) === 0) {
+      return signatures[s];
+    }
+  }
+}
 
 server.listen(80, _ => {
   console.log('listening')
@@ -39,5 +59,28 @@ server.listen(80, _ => {
 
   setInterval(_ => {
   //  io.emit('update img', brandonData)
-  }, 100)
+    NodeWebcam.capture( "test_picture", opts, (err, data) => {
+      //io.emit('update img', data)
+      jimp.read(data)
+      .then(image => {
+        image.mirror(true, false, (err, image) => {
+          image.getBase64Async(jimp.AUTO)
+          .then(base64 =>
+          {
+              io.emit('update img', base64)
+          })
+          .catch(err =>
+          {
+              console.error(err);
+          });
+        })
+
+        
+        //io.emit('update img', image.getBase64())
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    })
+  }, 500)
 })
