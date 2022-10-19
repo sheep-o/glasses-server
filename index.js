040d354fe12a2236cc215528e2816c1861fd8792
sheepo <47284480+sheep-o@users.noreply.github.com>
2022-10-19 08:48:57 -0400

    HELLO

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const app = express()
 const server = require('http').Server(app)
 const { Server } = require("socket.io");
 const fs = require('fs')
-const jimp = require('jimp')
-//const tesseract = require("node-tesseract-ocr")
 const NodeWebcam = require('node-webcam')
 const opts = {
-  width: 1280,
-  height: 720,
+  width: 720,
+  height: 10,
   quality: 10,
   frames: 1,
   delay: 0,
   saveShots: false,
-  callbackReturn: 'buffer',
+  callbackReturn: 'base64',
   device: false
 }
 const Webcam = NodeWebcam.create( opts )
@@ -26,61 +24,20 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   console.log('user connected')
-  
-  // const config = {
-  //   lang: "eng"
-  // }
-
-  // tesseract
-  //   .recognize('./eng_bw.png', config)
-  //   .then((text) => {
-  //     console.log("Result:", text)
-  //   })
-  //   .catch((error) => {
-  //     console.log(error.message)
-  //   })
-
 })
-
-function detectMimeType(b64) {
-  for (var s in signatures) {
-    if (b64.indexOf(s) === 0) {
-      return signatures[s];
-    }
-  }
-}
 
 server.listen(80, _ => {
   console.log('listening')
 
-  //brandonData = fs.readFileSync('brandonshoe.jpg').toString('base64')
-
-  toggle = false
-
   setInterval(_ => {
   //  io.emit('update img', brandonData)
-    NodeWebcam.capture( "test_picture", opts, (err, data) => {
+      NodeWebcam.capture( "test_picture", opts, (err, data) => {
       //io.emit('update img', data)
-      jimp.read(data)
-      .then(image => {
-        image.mirror(true, false, (err, image) => {
-          image.getBase64Async(jimp.AUTO)
-          .then(base64 =>
-          {
-              io.emit('update img', base64)
-          })
-          .catch(err =>
-          {
-              console.error(err);
-          });
-        })
-
-        
+        io.emit('update img', data)
         //io.emit('update img', image.getBase64())
       })
       .catch(err => {
         console.error(err)
       })
-    })
   }, 500)
 })
